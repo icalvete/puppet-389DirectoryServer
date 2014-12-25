7d7c05770bddf8108409af6a6f24bf217f0c7b98
Israel Calvete <icalvete@logtrust.com>
2014-12-25 10:50:39 +0100

    389ds was included is ubuntu 14.04

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,30 +1,5 @@
 class 389ds::install {
 
-  case $::operatingsystem {
-    /^(CentOS|RedHat)$/:{
-    }
-    /^(Debian|Ubuntu)$/: {
-
-      realize Package['python-software-properties']
-
-      exec {'add_389ds_repo':
-        command => '/usr/bin/add-apt-repository ppa:ubuntu-389-directory-server/ppa',
-        require => Package['python-software-properties'],
-        notify  => Exec['update_389ds_repo'],
-        unless  => "/usr/bin/test -f /etc/apt/sources.list.d/ubuntu-389-directory-server-ppa-${::lsbdistcodename}.list"
-      }
-
-      exec {'update_389ds_repo':
-        command     => '/usr/bin/apt-get update',
-        require     => Exec['add_389ds_repo'],
-        refreshonly => true,
-      }
-    }
-    default: {
-      fail("Operating System ${::operatingsystem} not supported")
-    }
-  }
-
   package {$389ds::params::fonts:
     ensure  => present
   }
